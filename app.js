276a405b9c3d70779df742e1982164eabe05af65
jennifershinshin <jennifershinshin@gmail.com>
2020-12-12 17:25:03 -0800

    made setting up local server and generating portfolio async. Not sure about the promise inside localServer

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
 const localServer = require("./localServer");
+const portfolio = require("./portfolio");
 
-const readline = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+async function runApp() {
+    try {
+        await localServer.setUpLocalServer();
+        portfolio.generatePortfolio();
+    }
+    catch(e) {
+        console.log('Error in runApp(): ', e);
+    }
+}
 
-readline.question("What is your name? ", function(name) {
-    readline.question("Where do you live? ", function(residence) {
-        readline.question("What is your occupation? ", function(occupation) {
-            readline.question("What is your LinkedIn? ", function(bio) {
-                readline.question("Please write a short bio of yourself: ", function(linkedIn) {
-                    readline.question("What are your hobbies? (Please separate them with commas) ", function(hobbies) {
-                        readline.question("What are your top 3 coding languages or skills? (Please separate them with commas) ", function(hobbies) {
-                            //generate html here?
-                            readline.close();
-                        });
-                    });
-                });
-            });
-        });
-    });
-});
-
-readline.on("close", function() {
-    process.exit(0);
-});
+runApp();
